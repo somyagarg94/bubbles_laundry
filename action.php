43006cf62a9a8046ba700b8f9729e9b36e23e65f
Somya Garg <somgarg@deloitte.com>
2019-09-07 17:14:41 -0400

    Add changes

--- action.php
+++ action.php
@@ -19,12 +19,16 @@ if(count($_POST)>0) {
   }
 
 // make query & get result
-$result = mysqli_query($conn,"SELECT * FROM users WHERE name='" . $_POST["uname"] . "' and password = '". $_POST["psw"]."' and role = '". $_POST["role"]."'");
+$result = mysqli_query($conn,"SELECT * FROM users WHERE name='" . $_POST["uname"] . "' and password = '". $_POST["psw"]."'");
 $count  = mysqli_num_rows($result);
 if($count==0) {
     $message = "Invalid Username or Password!";
 } else {
     $message = "You are successfully authenticated!";
+
+    if($_POST["role"] === "retailer"){
+        echo "<b>hi Retailer</b>";
+    }
 }
 }
 echo $message;
@@ -33,3 +37,6 @@ echo $message;
 
 </body>
 </html>
+
+
+"SELECT * FROM users WHERE user_name='" . $_POST["userName"] . "' and password = '". $_POST["password"]."' and role = '". $_POST["role"]."'"
